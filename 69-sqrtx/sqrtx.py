571620d60c32580e31a82ad456a22d66586bde08
abhisek7654gdd-svg <abhisek7654gdd@gmail.com>
2026-06-06 13:21:32 +0530

    Time: 11 ms (16.93%) | Memory: 12.5 MB (18.28%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 2:
+            return x
+
+        left, right = 2, x // 2
+        while left <= right:
+            mid = (left + right) // 2
+            sq = mid * mid
+            if sq == x:
+                return mid
+            elif sq < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return right
